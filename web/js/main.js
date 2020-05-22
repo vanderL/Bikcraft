433f09620792a500138a35ebb7a3e5a3859d5196
Vander Lima <vnd.vander@gmail.com>
2020-05-22 05:03:56 -0300

    Animações

--- web/js/main.js
+++ web/js/main.js
@@ -10,3 +10,28 @@ $(".rslides_portfolio").responsiveSlides({
   timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
 });
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".introducao h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao blockquote").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".introducao .btn").addClass("animated fadeInDown");
+	}, 1200);
+	setTimeout(function () {
+		$(".animar").addClass("animated fadeInDown");
+	}, 1600);
+
+	setTimeout(function () {
+		$(".introducao-interna h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao-interna p").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".animar-interno").addClass("animated fadeInDown");
+	}, 1200);
+});
